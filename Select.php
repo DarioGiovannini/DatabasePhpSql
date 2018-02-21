c298d05436b3defb4e101dce300d0b6af53be549
Fabio Fonti <fontiscuola@gmail.com>
2018-02-21 11:59:23 +0100

    select modificato

--- Select.php
+++ Select.php
@@ -9,18 +9,21 @@ if ($result->num_rows > 0) {
         $nome=$conn->real_escape_string(htmlentities($row["Nome"]));
         $cognome=$conn->real_escape_string(htmlentities($row["Cognome"]));
         echo "<tr>";
-        echo "<td name='id' onclick='Ordina(document.getElementsByTagName(\"tr\"),document.getElementsByName(\"id\"))'>" . $row["Id"]. "</td><td name='nome' onclick='Ordina(document.getElementsByTagName(\"tr\"),document.getElementsByName(\"nome\"))'>" . $row["Nome"]. "</td><td name='cognome' onclick='Ordina(document.getElementsByTagName(\"tr\"),document.getElementsByName(\"cognome\"))' >" . $row["Cognome"]. "</td><td name='Email'onclick='Ordina(document.getElementsByTagName(\"tr\"),document.getElementsByName(\"Email\"))'>" . $row["email"] . "</td><td>" ."<form action=\"Update.php\" method=\"get\">
+        echo "<td name='id' onclick='Ordina(document.getElementsByTagName(\"tr\"),document.getElementsByName(\"id\"))'>" . $row["Id"]. "</td><td name='nome' onclick='Ordina(document.getElementsByTagName(\"tr\"),document.getElementsByName(\"nome\"))'>" . $row["Nome"]. "</td><td name='cognome' onclick='Ordina(document.getElementsByTagName(\"tr\"),document.getElementsByName(\"cognome\"))' >" . $row["Cognome"]. "</td><td name='Email'onclick='Ordina(document.getElementsByTagName(\"tr\"),document.getElementsByName(\"Email\"))'>" . $row["email"] . "</td><td>" ."
         <label for='Update'></label>
         <input type='hidden' value='$id' name='Id'>
         <input type='hidden' value='$nome' name='Nome'>
         <input type='hidden' value='$cognome' name='Cognome'>
         <input type='hidden' value='$email' name='email'>
-        <input type=\"submit\" value=\"Update\" class=\"btn btn-warning\" name='Update' id='Update' >
-        </form>" ."</td><td>
-        <form action='Delete.php' method='get'>
+        <input type=\"submit\" value=\"Update\" class=\"btn btn-warning\" name='Update' id='Update' onclick='Update()'>
+        </td><td>
+        
+
+        
+        
         <label for='submit'></label>
         <input type='hidden' value='$id' name='Righe'>
-        <input type=\"submit\" value=\"Delete\" class=\"btn btn-danger\" name='submit' id='submit' > </form>" ."</td>" .
+        <input type=\"submit\" onclick='Delete(\"$id\")'value=\"Delete\" class=\"btn btn-danger\" name='submit' id='submit' >" ."</td>" .
             "</form>";
         echo "</tr>";
     }
