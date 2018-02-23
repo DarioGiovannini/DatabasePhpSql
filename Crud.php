cbb782af787a69d8ee8ea0ca0fe04654638d7cd0
DarioGiovannini <dario0giovannini@gmail.com>
2018-02-23 14:49:59 +0100

    Aggiunto jquery

--- Crud.php
+++ Crud.php
@@ -3,23 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>CRUD</title>
+    <title>Crud</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="css/Colori.css">
     <link rel="script" type="text/javascript" href="Script/Cerca.js">
     <script language="JavaScript" type="text/JavaScript" src="Script/JavaScript.js"></script>
 </head>
-<body onload="Select(document.getElementById('Tabella'));">
+<body onload="Select($('#Tabella')[0]);">
 <div class="gianni">
     <h1 align="center"> Database </h1>
 </div>
 <p align="right" id="p")>
-    <button class="btn btn-success glyphicon glyphicon-plus" onclick="Form(document.getElementById('p'),'Aggiungi',0);">Aggiungi</button>
+    <button class="btn btn-success glyphicon glyphicon-plus" onclick="Form($('#p')[0],'Aggiungi',0);">Aggiungi</button>
 </p>
 <table class='table table-hoover' id="Tabella">
 </table>
 <form>
-    <h4>Ricerca</h4> <input type="text" id="ricerca" onkeyup="Ricerca(document.getElementById('ricerca').value,document.getElementsByTagName('td'));">
+    <h4>Ricerca</h4> <input type="text" id="ricerca" onkeyup="Ricerca($('#ricerca')[0].value,$('td'));">
 </form>
 <script src="js/jquery-3.3.1.min.js"></script>
 <script src="js/bootstrap.js"></script>
--- Select.php
+++ Select.php
@@ -2,26 +2,36 @@
 include 'Connessione.php';
 $sql = "SELECT * FROM tabella";
 $result = $conn->query($sql);
-$text="";
+$text="<thead>         
+       <th> Nome    </th>
+       <th> Cognome </th>
+       <th> Email   </th>
+       <th> Update  </th>
+       <th> Delete  </th> 
+       </thead> ";
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         $id=$conn->real_escape_string(htmlentities($row["Id"]));
         $email=$conn->real_escape_string(htmlentities($row["email"]));
         $nome=$conn->real_escape_string(htmlentities($row["Nome"]));
         $cognome=$conn->real_escape_string(htmlentities($row["Cognome"]));
-        $text = $text . "<tr>"."<td name='id' onclick='Ordina(document.getElementsByTagName(\"tr\"),document.getElementsByName(\"id\"))'>" . $row["Id"]. "</td><td name='nome' onclick='Ordina(document.getElementsByTagName(\"tr\"),document.getElementsByName(\"nome\"))'>" . $row["Nome"]. "</td><td name='cognome' onclick='Ordina(document.getElementsByTagName(\"tr\"),document.getElementsByName(\"cognome\"))' >" . $row["Cognome"]. "</td><td name='Email'onclick='Ordina(document.getElementsByTagName(\"tr\"),document.getElementsByName(\"Email\"))'>" . $row["email"] . "</td><td>" . "
-        <label for='Update'></label>        
+        $text = $text  . "<tr>"."<td name='id' onclick='Ordina($(\"tr\"),$(\"td[name=\\\"id\\\"]\"))'>" .
+        $row["Id"]     . "</td><td name='nome' onclick='Ordina($(\"tr\"),$(\"td[name=\\\"nome\\\"]\"))'>" .
+        $row["Nome"]   . "</td><td name='cognome' onclick='Ordina($(\"tr\"),$(\"td[name=\\\"cognome\\\"]\"))' >" .
+        $row["Cognome"]. "</td><td name='Email'onclick='Ordina($(\"tr\"),$(\"td[name=\\\"Email\\\"]\"))'>" .
+        $row["email"]  . "</td><td>" .
+        "<label for='Update'></label>        
         <p>
         <input type='hidden' value='$nome' name='Nome'>
         <input type='hidden' value='$cognome' name='Cognome'>
         <input type='hidden' value='$email' name='email'>
         <input type='hidden' value='$id' name='Id'>
-        <button class=\"btn btn-warning glyphicon glyphicon-pencil\" name='Update' id='Update' onclick='Form(this.parentNode,\"Update\",$id)'> Update</button>
-        </p> " .
-        "</td><td>
+        <button class=\"btn btn-warning btn-block glyphicon glyphicon-pencil\" name='Update' id='Update' onclick='Form(this.parentNode,\"Update\",$id)'> Update</button>
+        </p>
+        </td><td>
         <label for='submit'></label>
         <input type='hidden' value='$id' name='Righe'>
-        <button class=\"btn btn-danger glyphicon glyphicon-minus\" name='submit' id='submit' onclick='Delete($id)'> Delete</button> </form>" ."</td>" ."</tr>";
+        <button class=\"btn btn-danger btn-block glyphicon glyphicon-minus\" name='submit' id='submit' onclick='Delete($id)'> Delete</button> </form>" ."</td>" ."</tr>";
     }
 } else {
     echo "Nessun Risultato";
